6bf3a3ae891080188e903c279b55cb3918e14d41
viet oba <buixuanviet.dev@gmail.com>
2020-06-01 10:05:06 +0700

    add all

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <Navbar/>
+        <ProductList/>
+        <Details/>
+        <Cart/>
+        <Default/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
